dc984be8fa7cb5fec9dcbc8f9f5e8d77e715b364
Bradley <gonzales.bradleyd@gmail.com>
2022-09-22 01:54:13 -0400

    Add tab switching and today task display

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export let taskTitle;
 export let taskDesc;
 export let taskDate;
 export let taskPrio;
-export let count = 1;
+export let count = 0;
 export let flag = false;
 const form = document.querySelectorAll('.form');
 export function collectData(e) {
@@ -18,7 +18,6 @@ export function collectData(e) {
     taskPrio = formData.get('priority');
     checkData();
     if (flag === false) {
-
         insertDataToWebpage()
     }
 }
@@ -48,29 +47,115 @@ export function checkData() {
     }
 }
 export function insertDataToWebpage() {
-    let newTask = new ToDoList(taskTitle, taskDesc, taskDate, taskPrio);
+    count++;
     const taskDiv = document.querySelectorAll('.task');
     const taskCard = document.createElement('div');
     taskCard.classList.add('taskCard');
     taskCard.setAttribute('id', `task${count}`)
     const titleElement = document.createElement('p');
+    titleElement.classList.add('tasktitle')
     const descElement = document.createElement('p');
+    descElement.classList.add('taskdesc')
     const dateElement = document.createElement('p');
+    dateElement.classList.add('taskdate')
     const prioElement = document.createElement('p');
-    titleElement.textContent = newTask.title;
-    descElement.textContent = newTask.description
-    dateElement.textContent = newTask.dueDate;
-    prioElement.textContent = newTask.priority
+    prioElement.classList.add('taskprio')
+    let newTask = new ToDoList(taskTitle, taskDesc, taskDate, taskPrio);
+    localStorage.setItem(`task${count}title`, newTask.title);
+    localStorage.setItem(`task${count}desc`, newTask.description);
+    localStorage.setItem(`task${count}date`, newTask.dueDate);
+    localStorage.setItem(`task${count}prio`, newTask.priority);
+    localStorage.setItem(`count`, count)
+    titleElement.textContent = localStorage.getItem(`task${count}title`)
+    descElement.textContent = localStorage.getItem(`task${count}desc`)
+    dateElement.textContent = localStorage.getItem(`task${count}date`)
+    prioElement.textContent = localStorage.getItem(`task${count}prio`)
     taskDiv.forEach(task => task.appendChild(taskCard));
     taskCard.appendChild(titleElement);
     taskCard.appendChild(descElement);
     taskCard.appendChild(dateElement);
     taskCard.appendChild(prioElement);
 
-    count++;
-
 }
+export function insertLocalStorageToWebpage() {
+    if (localStorage.length !== 0) {
+        for (let i = 1; i <= parseInt(localStorage.getItem('count')); i++) {
+            count = i;
+            const taskDiv = document.querySelectorAll('.task');
+            const taskCard = document.createElement('div');
+            taskCard.classList.add('taskCard');
+            taskCard.setAttribute('id', `task${i}`)
+            const titleElement = document.createElement('p');
+            titleElement.classList.add('tasktitle')
+            const descElement = document.createElement('p');
+            descElement.classList.add('taskdesc')
+            const dateElement = document.createElement('p');
+            dateElement.classList.add('taskdate')
+            const prioElement = document.createElement('p');
+            prioElement.classList.add('taskprio')
+            titleElement.textContent = localStorage.getItem(`task${i}title`)
+            descElement.textContent = localStorage.getItem(`task${i}desc`)
+            dateElement.textContent = localStorage.getItem(`task${i}date`)
+            prioElement.textContent = localStorage.getItem(`task${i}prio`)
+            let myDate = new Date();
+            let todayDate = new Date(myDate.getTime() + myDate.getTimezoneOffset() * 60000).toLocaleDateString();
+            console.log(todayDate);
+            console.log(localStorage.getItem('task1date'))
 
+            taskDiv.forEach(task => task.appendChild(taskCard));
+            taskCard.appendChild(titleElement);
+            taskCard.appendChild(descElement);
+            taskCard.appendChild(dateElement);
+            taskCard.appendChild(prioElement);
+        }
+    }
+    return;
+}
+export function showTodaysTasks() {
+    const taskDivParent = document.getElementById('tasks')
+    let child = taskDivParent.lastElementChild;
+    while (child) {
+        taskDivParent.removeChild(child);
+        child = taskDivParent.lastElementChild;
+    }
+    let myDate = new Date();
+    let todayDate = new Date(myDate.getTime() + myDate.getTimezoneOffset() * 60000).toLocaleDateString();
+    let todayTasks = [];
+    let todayTasksValues = {};
+    
+    for (let i = 1; i <= parseInt(localStorage.getItem('count')); i++) {
+        if (todayDate === localStorage.getItem(`task${i}date`)) {
+            const taskDiv = document.querySelectorAll('.task');
+            const taskCard = document.createElement('div');
+            taskCard.classList.add('taskCard');
+            taskCard.setAttribute('id', `task${count}`)
+            const titleElement = document.createElement('p');
+            titleElement.classList.add('tasktitle')
+            const descElement = document.createElement('p');
+            descElement.classList.add('taskdesc')
+            const dateElement = document.createElement('p');
+            dateElement.classList.add('taskdate')
+            const prioElement = document.createElement('p');
+            prioElement.classList.add('taskprio');
+            titleElement.textContent = localStorage.getItem(`task${i}title`)
+            descElement.textContent = localStorage.getItem(`task${i}desc`)
+            dateElement.textContent = localStorage.getItem(`task${i}date`)
+            prioElement.textContent = localStorage.getItem(`task${i}prio`)
+            todayTasksValues[`title${i}`] = localStorage.getItem(`task${i}title`)
+            todayTasksValues[`description${i}`] = localStorage.getItem(`task${i}desc`)
+            todayTasksValues[`duedate${i}`] = todayDate;
+            todayTasksValues[`priority${i}`] = localStorage.getItem(`task${i}prio`)
+            taskDiv.forEach(task => task.appendChild(taskCard));
+            taskCard.appendChild(titleElement);
+            taskCard.appendChild(descElement);
+            taskCard.appendChild(dateElement);
+            taskCard.appendChild(prioElement);
+        }
+        else {
+            console.log(`not the same`)
+        }
+    }
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css'
 import initModal from '../src/modal'
-import { collectData} from '../src/data'
+import { collectData, showTodaysTasks, insertLocalStorageToWebpage} from '../src/data'
 
 const addTaskButton = document.getElementsByClassName('imgbutton');
 const submitButton = document.querySelector('input[type=submit]')
@@ -8,3 +8,15 @@ Array.from(addTaskButton).forEach(button => {
     button.addEventListener('click', initModal());
 })
 submitButton.addEventListener('click', collectData);
+window.onload = function() {
+    insertLocalStorageToWebpage();
+}
+document.addEventListener('click', function (e) {
+    if (e.target.tagName === 'A' && e.target.textContent === 'Today') {
+        console.log(`clicked on today`);
+        showTodaysTasks();
+    }
+    if (e.target.tagName === 'A' && e.target.textContent === 'General') {
+        insertLocalStorageToWebpage();
+    }
+})
